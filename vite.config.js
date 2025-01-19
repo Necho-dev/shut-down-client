858623596f61f0216d1c75ff938d58cc523f79bb
Necho-dev <2839868773@qq.com>
2025-01-20 01:24:32 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import autoImportPlugin from '@opentiny/unplugin-tiny-vue'
 
 export default defineConfig ({
+  base: './',
+  resolve: {
+    alias: {
+      '@': './src'
+    }
+  },
   plugins: [
       vue(),
       autoImportPlugin('vite')
-  ]
+  ],
+  build: {
+    target: 'es2015'
+  }
 })
